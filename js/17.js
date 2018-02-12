eeb72d03eccf947c1c4a09cea8d1afc6c05b3b85
yunis <332963965@qq.com>
2018-02-12 16:53:56 +0800

    面向对象

--- js/17.js
+++ js/17.js
@@ -76,9 +76,64 @@ xiaoming.__proto__ === Student; // true
 // 构造函数
 
 
+function Student(name) {
+    this.name = name;
+    this.hello = function () {
+        alert('Hello, ' + this.name + '!');
+    }
+}
+var xiaoming = new Student('小明');
+xiaoming.name; // '小明'
+xiaoming.hello(); // Hello, 小明!
+/* 
+当使用 new 调用这个函数的时候，他就是一个构造函数，
+他绑定的 this 指向新创建的对象，并默认返回 this
+它的原型链：
+xiaoming ----> Student.prototype ----> Object.prototype ----> null
+*/ 
+xiaoming.constructor === Student.prototype.constructor; // true
+Student.prototype.constructor === Student; // true
+
+Object.getPrototypeOf(xiaoming) === Student.prototype; // true
+
+xiaoming instanceof Student; // true
+
+/*
+
+用new Student()创建的对象还从原型上获得了一个constructor属性，
+它指向函数Student本身：
+ */ 
 
 
+function Student(name) {
+    this.name = name;
+}
+
+Student.prototype.hello = function () {
+    alert('Hello, ' + this.name + '!');
+};
+/*
+要让创建的对象共享一个hello函数，
+根据对象的属性查找原则，
+我们只要把hello函数移动到xiaoming、xiaohong这些对象共同的原型上就可以了，
+也就是Student.prototype：
+
+？？？ OC 的类方法 ？？？
+
+ */ 
 
 
+/*
+原型链 
+需要看JavaScript高级程序设计，
+感觉这本书上面讲的比廖雪峰老师网站上讲的清楚。
+ */
 
+/*
 
+	JavaScript的原型继承实现方式就是：
+		1. 定义新的构造函数，并在内部用call()调用希望“继承”的构造函数，
+		并绑定this；
+		2. 借助中间函数F实现原型链继承，最好通过封装的inherits函数完成；
+		3. 继续在新的构造函数的原型上定义新方法。
+ */ 
